4113811294f9cc7c41012dd98b2353a4216ca4cd
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-10 15:03:29 +0330

    complete get started component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,35 @@
+import { discount, robot } from "../assets";
+import styles from "../style";
+import GetStarted from "./GetStarted";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className={`flex flex-col md:flex-row ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 px-6 sm:px-16`}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span> Payment Method
+          </h1>
+          <div className="ss:flex hidden mr-0 md:mr-4">
+            <GetStarted />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
