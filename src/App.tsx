abbd6fb2956bca4f3df4e6a91a3ea8aac0534398
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-10 12:11:37 +0330

    style file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <p className="text-8xl">Hello World</p>
-    </>
+    <div className="bg-primary w-full overflow-hidden">
+      <p className="text-8xl text-lime-400">Hello World</p>
+    </div>
   );
 }
 
