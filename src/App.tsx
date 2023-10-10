933e83a2ad139e2250bac11f4a1762522b55244c
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-10 13:25:11 +0330

    create and import main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+import styles from "./style";
+
+// Components
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Client,
+  CTA,
+  Footer,
+  NavBar,
+  Stats,
+  Testimonials,
+  Hero,
+} from "./components";
+
 function App() {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-      <p className="text-8xl text-lime-400">Hello World</p>
+    <div className={`min-h-screen bg-primary w-full overflow-hidden`}>
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+      {/* Navbar */}
+
+      {/* Main */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      {/* Main */}
+
+      {/* Content */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Client />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+      {/* Content */}
     </div>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import CardDeal from "./CardDeal";
+
+export {
+  NavBar,
+  Billing,
+  CardDeal,
+  Business,
+  Client,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
