2d25bf844c5133aa4e1fe84bac77b25a1e53d771
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-10 15:23:09 +0330

    hero section completed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,11 +33,22 @@ const Hero = () => {
           Payment Method
         </h1>
 
-        <p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="robot" className="w-full h-full relative z-[5]" />
+        <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient"></div>
+        <div className="absolute z-1 w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
+        <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20  blue__gradient"></div>
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
       </div>
     </section>
   );
