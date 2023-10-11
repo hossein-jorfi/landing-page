72f1f8df71bd92b20767fed6447dc39bc7a28174
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-11 08:34:22 +0330

    billing section created

--- src/components/Billing.tsx
+++ src/components/Billing.tsx
@@ -1,7 +1,16 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+
 const Billing = () => {
   return (
-    <div>Billing</div>
-  )
-}
+    <section id="product" className={layout.sectionImgReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="w-full h-full relative z-5" />
+        <div className="absolute z-[3] -left-1/2 top-0 w-1/2 h-1/2 rounded-full white__gradient"></div>
+        <div className="absolute z-0 -left-1/2 bottom-0 w-1/2 h-1/2 rounded-full blue__gradient"></div>
+      </div>
+    </section>
+  );
+};
 
-export default Billing
+export default Billing;
